f6f532fe89feeb2f63052883a1c3c31eafeb584f
Fabian Wenzelmann <fabianwenzelmann@posteo.de>
2020-03-21 00:44:45 +0100

    Add base64 encoder / decoder

---
+++ base64.go
@@ -0,0 +1,49 @@
+// Copyright 2020 Fabian Wenzelmann <fabianwen@posteo.eu>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gophc
+
+import "encoding/base64"
+
+// base64 provides the internal base64 encoding / decoding.
+// The base64 encoding / decoding is inspired by https://github.com/golang/crypto/blob/master/bcrypt/base64.go
+
+// DefaultAlphabet is the alphabet used by most algorithms.
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+var bcEncoding = base64.NewEncoding(alphabet)
+
+// Base64Encode encodes the source to base64 using the alphabet.
+func Base64Encode(src []byte) []byte {
+	n := bcEncoding.EncodedLen(len(src))
+	dst := make([]byte, n)
+	bcEncoding.Encode(dst, src)
+	for n > 0 && dst[n-1] == '=' {
+		n--
+	}
+	return dst[:n]
+}
+
+// Base64Decode decodes the source using the alphabet.
+func Base64Decode(src []byte) ([]byte, error) {
+	numOfEquals := 4 - (len(src) % 4)
+	for i := 0; i < numOfEquals; i++ {
+		src = append(src, '=')
+	}
+	dst := make([]byte, bcEncoding.DecodedLen(len(src)))
+	n, err := bcEncoding.Decode(dst, src)
+	if err != nil {
+		return nil, err
+	}
+	return dst[:n], nil
+}
