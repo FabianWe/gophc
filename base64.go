a2ec8ad11911357df31f9c50c717a851924119ff
Fabian Wenzelmann <fabianwenzelmann@posteo.de>
2020-12-14 20:59:50 +0100

    Add base64 encoder / decoder

---
+++ base64.go
@@ -0,0 +1,57 @@
+// Copyright 2020 Fabian Wenzelmann <fabianwen@posteo.eu>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gophc
+
+import (
+	"encoding/base64"
+)
+
+// base64 provides the internal base64 encoding / decoding.
+// The base64 encoding / decoding is inspired by https://github.com/golang/crypto/blob/master/bcrypt/base64.go
+
+// DefaultAlphabet is the alphabet used for phc.
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+var nonStrictEncoding = base64.NewEncoding(alphabet).WithPadding(base64.NoPadding)
+var strictEncoding = nonStrictEncoding.Strict()
+
+// Base64Encode encodes the source to base64 using the alphabet.
+func Base64Encode(src []byte) []byte {
+	encodeLen := strictEncoding.EncodedLen(len(src))
+	dst := make([]byte, encodeLen)
+	strictEncoding.Encode(dst, src)
+	return dst[:encodeLen]
+}
+
+func base64DecodeFromEncoding(enc *base64.Encoding, src []byte) ([]byte, error) {
+	dst := make([]byte, enc.DecodedLen(len(src)))
+	n, err := enc.Decode(dst, src)
+	if err != nil {
+		return nil, err
+	}
+	return dst[:n], nil
+}
+
+// Base64Decode decodes the source using the alphabet.
+func Base64Decode(src []byte) ([]byte, error) {
+	return base64DecodeFromEncoding(strictEncoding, src)
+}
+
+// Base64DecodeNotStrict decodes the source using the alphabet.
+//
+// In contrast to Base64Decode this method also allows non-zero trailing padding bits.
+func Base64DecodeNotStrict(src []byte) ([]byte, error) {
+	return base64DecodeFromEncoding(nonStrictEncoding, src)
+}
---
+++ tests/base64_test.go
@@ -0,0 +1,138 @@
+package tests
+
+import (
+	"bytes"
+	"github.com/FabianWe/gophc"
+	"testing"
+)
+
+// Note for anyone reading the tests: Never use bytes.Equal for comparing hashes of passwords,
+// use crypto/subtle/ (constant time compares)
+
+func TestBase64Encode(t *testing.T) {
+	// tests: complete groups (len % 3 == 0)
+	// and also mod 3 == 1 and 2
+	// also tests different trailing bits
+	tests := []struct {
+		in       []byte
+		expected string
+	}{
+		{
+			nil,
+			"",
+		},
+		{
+			[]byte{136, 116, 131, 60, 124, 212, 132, 252, 202, 238, 176, 156, 38, 3, 133, 126},
+			"iHSDPHzUhPzK7rCcJgOFfg",
+		},
+		{
+			[]byte{7, 5, 32, 36, 113, 208, 107, 41, 196, 249, 174, 39, 102, 182, 17, 204, 233, 101, 25, 40, 227, 198, 236, 77, 95, 28, 141, 69, 75, 72, 232, 57, 112, 255, 57, 91, 183, 163, 243, 56, 191, 14, 157, 250, 172, 233, 55, 6},
+			"BwUgJHHQaynE+a4nZrYRzOllGSjjxuxNXxyNRUtI6Dlw/zlbt6PzOL8Onfqs6TcG",
+		},
+		{
+			[]byte{177, 25, 71, 133, 25, 138, 80, 108, 221, 58, 101, 231, 211, 85, 230, 95, 40, 29, 228, 167},
+			"sRlHhRmKUGzdOmXn01XmXygd5Kc",
+		},
+		{
+			[]byte{225, 245, 215, 27, 75, 41, 7, 221, 150, 60, 29, 77, 138, 212, 252, 252, 225, 244, 84, 162},
+			"4fXXG0spB92WPB1NitT8/OH0VKI",
+		},
+		{
+			[]byte{30, 62, 126, 118, 194, 180, 101, 0},
+			"Hj5+dsK0ZQA",
+		},
+	}
+
+	for _, tc := range tests {
+		encoded := string(gophc.Base64Encode(tc.in))
+		if encoded != tc.expected {
+			t.Errorf("base64 encoding error for input %v: Expected %s, got %s",
+				tc.in, tc.expected, encoded)
+		}
+
+	}
+}
+
+var base64DecodingTests = []struct {
+	in       string
+	expected []byte
+	strict   bool
+}{
+	{
+		"",
+		nil,
+		true,
+	},
+	{
+		"iHSDPHzUhPzK7rCcJgOFfg",
+		[]byte{136, 116, 131, 60, 124, 212, 132, 252, 202, 238, 176, 156, 38, 3, 133, 126},
+		true,
+	},
+	{
+		"BwUgJHHQaynE+a4nZrYRzOllGSjjxuxNXxyNRUtI6Dlw/zlbt6PzOL8Onfqs6TcG",
+		[]byte{7, 5, 32, 36, 113, 208, 107, 41, 196, 249, 174, 39, 102, 182, 17, 204, 233, 101, 25, 40, 227, 198, 236, 77, 95, 28, 141, 69, 75, 72, 232, 57, 112, 255, 57, 91, 183, 163, 243, 56, 191, 14, 157, 250, 172, 233, 55, 6},
+		true,
+	},
+	{
+		"sRlHhRmKUGzdOmXn01XmXygd5Kc",
+		[]byte{177, 25, 71, 133, 25, 138, 80, 108, 221, 58, 101, 231, 211, 85, 230, 95, 40, 29, 228, 167},
+		true,
+	},
+	{
+		"4fXXG0spB92WPB1NitT8/OH0VKI",
+		[]byte{225, 245, 215, 27, 75, 41, 7, 221, 150, 60, 29, 77, 138, 212, 252, 252, 225, 244, 84, 162},
+		true,
+	},
+	// two invalid ones (they have unprocessed bits that are not zero)
+	{
+		"Hj5+dsK0ZR",
+		[]byte{30, 62, 126, 118, 194, 180, 101},
+		false,
+	},
+	{
+		"Hj5+dsK0ZQB",
+		[]byte{30, 62, 126, 118, 194, 180, 101, 0},
+		false,
+	},
+}
+
+func TestBase64Decode(t *testing.T) {
+	for _, tc := range base64DecodingTests {
+		decoded, decodeErr := gophc.Base64Decode([]byte(tc.in))
+		if tc.strict {
+			// should process without errors
+			if decodeErr != nil {
+				t.Errorf("unexpected error decoding base64 string: input \"%s\", error was %v",
+					tc.in, decodeErr)
+				continue
+			}
+			// now test if we got expected
+			if !bytes.Equal(decoded, tc.expected) {
+				t.Errorf("base64 decoding failed for input \"%s\": expected %v, got %v",
+					tc.in, tc.expected, decoded)
+			}
+		} else {
+			// should give an error
+			if decodeErr == nil {
+				t.Errorf("base64 decoding should fail in strict mode for input \"%s\", but got %v as result",
+					tc.in, decoded)
+			}
+		}
+	}
+}
+
+func TestBase64DecodeNotStrict(t *testing.T) {
+	for _, tc := range base64DecodingTests {
+		// should not fail on any of the inputs
+		decoded, decodeErr := gophc.Base64DecodeNotStrict([]byte(tc.in))
+		if decodeErr != nil {
+			t.Errorf("unexpected error decoding base64 (not strict): input \"%s\", error was %v",
+				tc.in, decodeErr)
+			continue
+		}
+		if !bytes.Equal(decoded, tc.expected) {
+			t.Errorf("base64 decoding (not strict) failed for input \"%s\": expected %v, got %v",
+				tc.in, tc.expected, decoded)
+		}
+	}
+}
